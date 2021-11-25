2e20ae9c46642c2d3b566fd66583f0b86bda7d36
Brennan McCreary <brennanmccreary@gmail.com>
2021-11-24 21:40:19 -0500

    Add declarations for arrays and variables for options and choices. Add log of choices.

--- app.js
+++ app.js
@@ -7,3 +7,28 @@ function selectRandomArrayIndex(arr) { //returns random number for an index of a
     result = (Math.floor(Math.random() * max)); 
     return result;
 }
+
+//declare arrays with lists for destinations, restaurants, transportation, and entertainment
+let destinationsArr = 
+["Miami","New York City","Seattle","Nashville","Pittsburgh","Indianapolis","Charleston","Baltimore","Chicago","Houston"];
+let restaurantsArr = 
+["Red Robin","Applebee's","Chili's","Wendy's","Hibachi","Texas Roadhouse"];
+let transportationArr = 
+["Walk","Bus","Rickshaw","Taxi","Ride Share"];
+let entertainmentArr = 
+["Comedy Show","Movie","City Tour","Magic Show","Sporting Event"];
+
+//declare initial randomized variable choices
+let destinationsChoice = destinationsArr[selectRandomArrayIndex(destinationsArr)];
+let restaurantsChoice = restaurantsArr[selectRandomArrayIndex(restaurantsArr)];
+let transportationChoice = transportationArr[selectRandomArrayIndex(transportationArr)];
+let entertainmentChoice = entertainmentArr[selectRandomArrayIndex(transportationArr)];
+
+//display initial choices
+console.log
+(`Your generated day trip:
+Destination: ${destinationsChoice}
+Restauraunt: ${restaurantsChoice}
+Transportaiton: ${transportationChoice}
+Entertainment: ${entertainmentChoice}`);
+
